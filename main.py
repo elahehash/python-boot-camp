6945b563f7d210f99e70ab98d63185bdf19ff778
elahehash <elaheh.bonab@emu.edu.tr>
2024-05-19 21:26:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city=input("whats the name of your city you grew up in?\n")
+pet=input("whats your pets name?\n")
+print("your brand name could be:" + city + " " + pet)
